75b41d3f53fa60f6b6ba5b3e1bde792b70c39d9e
Lucas Assis Lima <lucasssi89@gmail.com>
2022-03-06 20:47:36 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/trinove/TrinoveSistemasApplication.java
+++ src/main/java/br/com/trinove/TrinoveSistemasApplication.java
@@ -13,6 +13,7 @@ import br.com.trinove.domain.Cidade;
 import br.com.trinove.domain.Cliente;
 import br.com.trinove.domain.Endereco;
 import br.com.trinove.domain.Estado;
+import br.com.trinove.domain.ItemPedido;
 import br.com.trinove.domain.Pagamento;
 import br.com.trinove.domain.PagamentoComBoleto;
 import br.com.trinove.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.trinove.repositories.CidadeRepository;
 import br.com.trinove.repositories.ClienteRepository;
 import br.com.trinove.repositories.EnderecoRepository;
 import br.com.trinove.repositories.EstadoRepository;
+import br.com.trinove.repositories.ItemPedidoRepository;
 import br.com.trinove.repositories.PagamentoRepository;
 import br.com.trinove.repositories.PedidoRepository;
 import br.com.trinove.repositories.ProdutoRepository;
@@ -48,6 +50,9 @@ public class TrinoveSistemasApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	
 	
 	
@@ -126,7 +131,19 @@ public class TrinoveSistemasApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 		
+		ped1.getItens().addAll(Arrays.asList(ip2,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 		
 		
